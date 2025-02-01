68150346660f893d9674e29100db77908993f99a
Hongde Construction LLC <hongdebuild@gmail.com>
2025-02-01 17:24:19 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,3 @@ function toggleMenu() {
         navMenu.style.display = 'flex';
     }
 }
-
-// 获取footer元素
-const footer = document.querySelector('footer');
-
-// 当鼠标移到footer上时，隐藏footer
-footer.addEventListener('mouseover', () => {
-    footer.classList.add('hidden');
-});
-
-// 当鼠标移开footer时，显示footer
-footer.addEventListener('mouseout', () => {
-    footer.classList.remove('hidden');
-});
